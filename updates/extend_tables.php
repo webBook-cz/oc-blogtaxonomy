2f98edd0b3090b039371ffb87e754c4006ac5179
webBook <info@webbook.page>
2020-08-31 17:55:02 +0200

    Create extend_tables.php

---
+++ updates/extend_tables.php
@@ -0,0 +1,23 @@
+<?php namespace Webbook\BlogTaxonomy\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class ExtendTables extends Migration
+{
+    public function up()
+    {
+        Schema::table('rainlab_blog_posts', function($table) {
+            $table->engine = 'InnoDB';    
+            $table->integer('author_id')->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('rainlab_blog_posts', function($table) {
+            $table->dropColumn('author_id');           
+        });
+    }
+
+}
